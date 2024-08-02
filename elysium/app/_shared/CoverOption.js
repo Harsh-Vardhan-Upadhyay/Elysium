739676c46ea3b39f7dd9ba2b9efdb7fa6ef8469f
Harsh Vardhan Upadhyay <91301778+Harsh-Vardhan-Upadhyay@users.noreply.github.com>
2024-08-02 13:59:36 +0530

    added covers for workspace

---
+++ elysium/app/_shared/CoverOption.js
@@ -0,0 +1,21 @@
+export default[
+
+    {
+        imageUrl: 'https://i.pinimg.com/564x/7b/89/c9/7b89c96e243c25def2e66134eeb3cfd5.jpg'
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/564x/ca/92/41/ca9241baa54c87c730186b487aad53c3.jpg'
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/564x/7a/88/5a/7a885acdb0a211b7c720d7839625290c.jpg'
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/564x/b6/b6/d8/b6b6d866be70b29dd64e25bdc56ab9bf.jpg'
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/564x/ab/9a/41/ab9a410821fc62fc7a3c51eccf10bf19.jpg'
+    },
+    {
+        imageUrl: 'https://i.pinimg.com/564x/db/34/3b/db343b386dbd4b3fdcb305bb72c41f6a.jpg'
+    },
+]
